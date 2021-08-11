064618a2accb173634e37379eb3e33c5f84091c9
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2021-08-11 07:46:08 +0530

    Time: 24 ms (26.87%), Space: 11 MB (22.51%) - LeetHub

--- flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int flip=0,cnt=0;
+        int flip=0,cnt=0; //count 0s & 1s
         for(auto i:s){
-            if(i=='0')
-                flip++;
+            // if(i=='0')
+            //     flip++;
+            
+            if(i=='0'){
+                if(cnt!=0)
+                    flip++;
+            }
             else cnt++;
             
             flip=min(flip,cnt);
