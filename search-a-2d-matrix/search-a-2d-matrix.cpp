e6a1766a20b7e484fd9bb13f5953f8486a12da13
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2021-08-24 23:36:17 +0530

    Time: 4 ms (74.11%), Space: 9.7 MB (5.33%) - LeetHub

--- search-a-2d-matrix/search-a-2d-matrix.cpp
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -6,14 +6,28 @@ public:
         
         int i=0,j=n-1;
         
-    while(i<m && j>=0){
-        if(matrix[i][j]==target)
-            return true;
-        else if(matrix[i][j]>target)
-            j--;
-        else                // matrix[i][j]<target
-            i++;
-    }
+    // while(i<m && j>=0){
+    //     if(matrix[i][j]==target)
+    //         return 1;
+    //     else if(matrix[i][j]>target)
+    //         j--;
+    //     else                // matrix[i][j]<target
+    //         i++;
+    // }
+        
+        int lo=0,hi=n-1;
+        while(i<m && lo<=hi){
+            if(matrix[i][hi]<target){
+                i++; continue;
+                }
+            
+            int mid=(lo+hi)/2;
+            if(matrix[i][mid]==target) return 1;
+            else if(matrix[i][mid]>target)
+                hi=mid-1;
+            else                // matrix[i][j]<target
+                lo=mid+1;
+        }
         
         return false;
     }
