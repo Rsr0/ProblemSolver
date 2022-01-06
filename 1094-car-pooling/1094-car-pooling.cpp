fda590989ca1e8ac2660a0c69d35f109c8ade779
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-01-06 17:07:46 +0530

    Time: 27 ms (18.02%), Space: 9.9 MB (83.11%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-  
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        if(trips.size()==1) return trips[0][0]<=capacity;
         int track[1001]={};
-        
+        int start=1000, end=0;
         for(auto t:trips){
-            track[t[1]]+=t[0]; // seat filled 
-            track[t[2]]-=t[0]; // seat left
+            track[t[1]]+=t[0]; //seats filled
+            track[t[2]]-=t[0]; //seats left
+            
+            start=min(start, t[1]);
+            end=max(end, t[2]);
         }
         
-        for(int i=0;i<1001;i++){
-            if(capacity<0) break;
+        for(int i=start;i<=end;i++){
             capacity-=track[i];
+            if(capacity<0) return 0;    
         }
-        
         return capacity>=0;
-      
     }
 };
