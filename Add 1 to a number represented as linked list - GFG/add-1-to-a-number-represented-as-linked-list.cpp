c9dddfb1b4df8c98418d30e35fc51a0688f29b84
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-01-10 17:18:29 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.cpp
@@ -0,0 +1,106 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* addOne(Node *head) 
+    {
+        // Your Code here
+        // return head of list after adding one
+        Node* curr=head, *temp=head;
+        while(curr->next){
+            if(curr->data<9 && curr->next->data==9)
+                temp=curr; //before 9 => 189 i.e, 8
+            curr=curr->next;
+        }
+        
+        if(curr->data==9){
+            if(temp->data==9){ // 999
+                temp=new Node(0);
+                temp->next=head;
+                head=temp;
+            }
+            
+            temp->data++;
+            temp=temp->next;
+            while(temp){
+                temp->data=0;
+                temp=temp->next;
+            }
+            
+        }
+        else 
+            curr->data++;
+            
+        return head;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
