0981374cb2c20d1eeec690f3d16c512ce9b806fb
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-01-18 12:06:09 +0530

    Time: 43 ms (5.42%), Space: 20.4 MB (20.95%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -8,20 +8,15 @@ public:
         if(sz==0) return 0;
         if(sz==1) return flowerbed[0]==0;
             
-        // add 0s on start and end
-        flowerbed.push_back(0);
-        flowerbed.insert(flowerbed.begin(),0);
-        sz+=2;
-        int i=1, cnt=0;
-        
-        // check if there are 3 0s in row
-        while(i<sz-1 && cnt<n){
-            if(flowerbed[i-1]==0 && flowerbed[i]==0 && flowerbed[i+1]==0){
+       int cnt=0;
+        for(int i=0;i<sz;i++){
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==sz-1 || flowerbed[i+1]==0)){
                 cnt++;
                 flowerbed[i]=1;
             }
-            i++;
+            if(cnt>=n)
+                return 1;
         }
-        return cnt==n;
+        return 0;
     }
 };
