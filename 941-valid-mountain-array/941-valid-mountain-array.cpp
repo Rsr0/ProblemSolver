cb8eb9d20753334966733847a7970cd251725031
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-01-25 17:03:38 +0530

    Time: 46 ms (27.60%), Space: 22.4 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -3,8 +3,7 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int n=arr.size();
         if(n<3) return 0;
-        
-        bool flag=0;
+
         int i=1;
         while(i<n && arr[i-1]<arr[i]){
             i++;
@@ -15,8 +14,8 @@ public:
         while(i<n && arr[i-1]>arr[i]){
             i++;
         }
-        if(i==n)
-            return 1;
-        return 0;
+        
+        return (i==n)? 1:0;
+   
     }
 };
