be6669c34a140ac2a84542195bebd5fba9b177e6
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-01-26 12:27:01 +0530

    Time: 276 ms (14.81%), Space: 85.3 MB (58.22%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,24 +11,20 @@
  */
 class Solution {
 public:
+    vector<int> ans;
     
-    void traverse(TreeNode*root, vector<int>& v){
+    void traverse(TreeNode*root){
         if(!root) return;
-        
-        v.push_back(root->val);
-        traverse(root->left,v);
-        traverse(root->right,v);
+        ans.push_back(root->val);
+        traverse(root->left);
+        traverse(root->right);
     }
     
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> v1, v2;
-        traverse(root1, v1);
-        traverse(root2, v2);
+        traverse(root1);
+        traverse(root2);
         
-        for(auto it:v2)
-            v1.push_back(it);
-        
-        sort(v1.begin(), v1.end());
-        return v1;
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
