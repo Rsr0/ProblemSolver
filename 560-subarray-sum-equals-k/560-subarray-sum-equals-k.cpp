8e4607aa74190a7e7bd7e66e9d0788bd97e9bb02
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-02-10 17:06:39 +0530

    Time: 96 ms (63.45%), Space: 36 MB (87.06%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,11 +4,13 @@ public:
         int n=nums.size();
         unordered_map<int,int> mp;
         int sum=0, count=0;
-        mp[0]=1;
+        mp[0]=1; // sum==k
         
         for(int i=0;i<n;i++){
             sum+=nums[i];
-         
+            
+           
+            
             if(mp.find(sum-k)!=mp.end())
                 count+=mp[sum-k];
         
