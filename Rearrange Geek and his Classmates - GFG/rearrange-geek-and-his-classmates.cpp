2894576b27511c9d7308ba8e51868eebf89268f9
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-02-18 09:35:14 +0530

    Added solution - LeetHub

--- Rearrange Geek and his Classmates - GFG/rearrange-geek-and-his-classmates.cpp
+++ Rearrange Geek and his Classmates - GFG/rearrange-geek-and-his-classmates.cpp
@@ -9,12 +9,26 @@ using namespace std;
 
 class Solution {
   public:
+  
+  /*
+  idx=a[i];
+  num=a[idx] % n; // original number
+  a[i] += num * n  
+  
+  a[i]=a[i]/n  // modified
+  */
+  
     void prank(long long a[], int n){
         // code here
-        vector<int> b(a,a+n);
+        for(int i=0;i<n;i++){
+            long long idx=a[i];
+            long long num=a[idx] % n;
+            a[i]+= num * n;
+        }
         
-        for(int i=0;i<n;i++)
-            a[i]=b[b[i]];
+        for(int i=0;i<n;i++){
+            a[i]=a[i]/n;
+        }
     }
 };
 
