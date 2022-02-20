c0dc2bf86edaa60666dc0d8ed8eee5f9df062839
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-02-20 19:32:23 +0530

    Time: 19 ms (37.59%), Space: 21.3 MB (22.51%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -11,25 +11,24 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root, int &ans, int i){
+    int cam=0;
+    int solve(TreeNode* root){
         if(!root) return 1;
-        int l=solve(root->left, ans, 1);
-        int r=solve(root->right, ans, 1);
+        int l=solve(root->left);
+        int r=solve(root->right);
         
-        if(l==0 || r==0){
-            ans++;
+        if(l==0 || r==0){ // add a camera
+            cam++;       
             return 2;
         }
-        if(l==2 || r==2)
+        else if(l==2 || r==2) //  covered with camera
             return 1;
-        else if(i==0)
-            ans++;
-        return 0;
+        else
+        return 0;  // not covered
     }
     
     int minCameraCover(TreeNode* root) {
-        int ans=0;
-        solve(root,ans,0);
-        return ans;
+   
+        return solve(root)==0 ? cam+1 : cam ;
     }
 };
