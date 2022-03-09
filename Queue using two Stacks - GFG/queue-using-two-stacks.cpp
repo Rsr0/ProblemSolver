cddff7244b9552d843c77fc238b9f4ad37dc094e
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-03-09 08:20:28 +0530

    Added solution - LeetHub

--- Queue using two Stacks - GFG/queue-using-two-stacks.cpp
+++ Queue using two Stacks - GFG/queue-using-two-stacks.cpp
@@ -66,20 +66,18 @@ void StackQueue :: push(int x)
 int StackQueue :: pop()
 {
         // Your Code      
+
+    if(s2.empty()){
+        if(s1.empty())
+            return -1;
+        
         while(!s1.empty()){
             s2.push(s1.top());
             s1.pop();
         }
-    
-    int x=-1;
-    if(!s2.empty()){
-        x=s2.top();
-        s2.pop();
     }
     
-    while(!s2.empty()){
-        s1.push(s2.top());
-        s2.pop();
-    }    
-        return x;
+    int x=s2.top();
+    s2.pop();
+    return x;
 }
