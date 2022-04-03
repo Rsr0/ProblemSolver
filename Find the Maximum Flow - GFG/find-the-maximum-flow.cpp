0f765c5041ce0f4b9700011427f532302828b585
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-04-03 17:52:23 +0530

    Added solution - LeetHub

--- Find the Maximum Flow - GFG/find-the-maximum-flow.cpp
+++ Find the Maximum Flow - GFG/find-the-maximum-flow.cpp
@@ -6,8 +6,7 @@ using namespace std;
 class Solution
 {
 public:
-int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N)
-   {
+int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N){
        int min_cap=INT_MAX;
        fill(parent.begin(), parent.end(), -1);
        vector<bool> visited(N,false);
@@ -17,19 +16,15 @@ int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N
        visited[source]=true;
        parent[source]=-1;
        
-       while(q.empty()==false)
-       {
+       while(!q.empty()){
            auto u=q.front();
            q.pop();
            
            //adjacents
            
-           for(int v=0;v<N;v++)
-           {
-               if(visited[v]==false && graph[u][v]!=0)
-               {
-                   if(v==sink)
-                   {
+           for(int v=0;v<N;v++){
+               if(visited[v]==false && graph[u][v]!=0){
+                   if(v==sink){
                        parent[v]=u;
                        min_cap=min(min_cap,graph[u][v]);
                        return min_cap;
@@ -46,23 +41,18 @@ int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N
        return 0;
    }
    
-    int fordFulkerson(vector<vector<int>> &graph,int source,int sink,int N)
-   {
+    int fordFulkerson(vector<vector<int>> &graph,int source,int sink,int N){
        vector<int> parent(N,-1);
-       
        int res=0;
        
-       while(BFS(graph,parent,source,sink,N)!=0)
-       {
+       while(BFS(graph,parent,source,sink,N)!=0){
            int min_cap=BFS(graph,parent,source,sink,N);
-           
            res+=min_cap;
            
            //considering all the edged in this path.
            
            int v=sink;
-           while(v!=source)
-           {
+           while(v!=source){
                int u=parent[v];
                graph[u][v]-=min_cap;
                graph[v][u]+=min_cap;
@@ -73,23 +63,18 @@ int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N
        return res;
    }
    
-    int solve(int N,int M,vector<vector<int>> Edges)
-    {
+    int solve(int N,int M,vector<vector<int>> Edges){
         // code here
-           vector<vector<int>> graph;
+       vector<vector<int>> graph(N,vector<int>(N,0));
+    //   for(int i=0;i<N;i++){
+    //       vector<int> v1;
+    //       for(int j=0;j<N;j++)
+    //           v1.push_back(0);
+          
+    //       graph.push_back(v1);
+    //   }
        
-       for(int i=0;i<N;i++)
-       {
-           vector<int> v1;
-           for(int j=0;j<N;j++)
-           {
-               v1.push_back(0);
-           }
-           graph.push_back(v1);
-       }
-       
-       for(auto x: Edges)
-       {
+       for(auto x: Edges){
            int u=x[0]-1;
            int v=x[1]-1;
            int wt=x[2];
@@ -97,9 +82,8 @@ int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N
            graph[u][v]+=wt;
            graph[v][u]+=wt;
        }
-       
+     
        return fordFulkerson(graph,0,N-1,N);
-
     }
 };
 
