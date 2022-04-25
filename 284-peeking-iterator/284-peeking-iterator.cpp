e466aa79c666e88358749cd4a6a8e04b0ef89d83
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-04-25 08:31:03 +0530

    Time: 2 ms (79.45%), Space: 7.9 MB (6.21%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.cpp
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -19,27 +19,25 @@
 
 class PeekingIterator : public Iterator {
 public:
-    vector<int> it;
-    int i=0;
 	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
 	    // Initialize any member here.
 	    // **DO NOT** save a copy of nums and manipulate it directly.
 	    // You should only use the Iterator interface methods.
-	    it=nums;
+	    
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	int peek() {
-        return it[i];
+        return Iterator(*this).next();
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	int next() {
-	    return it[i++];
+	    return Iterator::next();
 	}
 	
 	bool hasNext() const {
-	    return i<it.size();
+	    return Iterator::hasNext();
 	}
 };
