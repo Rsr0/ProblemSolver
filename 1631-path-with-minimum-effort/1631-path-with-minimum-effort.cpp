04f943c82feb23ead85988900bacce6d68066779
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-04-28 12:56:11 +0530

    Time: 167 ms (88.28%), Space: 20 MB (82.16%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
+
+#define pii pair<int, pair<int,int>>
+
     int dx[4] = {0, 0, -1, 1};
     int dy[4] = {1, -1, 0, 0};
     int n,m;
+    
     bool isValid(int x, int y){
         return (x>=0 && y>=0 && x<n && y<m);
     }
     
-    bool bfs(vector<vector<int>>& heights, int k){
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        vector<vector<bool>> vis(n, vector<bool>(m, 0));
-        vis[0][0]=1;
-        while(!q.empty()){
-            auto curr=q.front();
-            q.pop();
-            int cx = curr.first, cy = curr.second;
-            if(cx==n-1 && cy==m-1)  return 1;
+    int minimumEffortPath(vector<vector<int>>& heights) {
+       n=heights.size(), m=heights[0].size();
+       priority_queue<pii, vector<pii>, greater<pii>> pq;
+       pq.push({0,{0,0}});
+       vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
+       dis[0][0]=0;
+        while(!pq.empty()){
+            auto curr=pq.top();
+            pq.pop();
+            int d=curr.first, r=curr.second.first, c=curr.second.second;
+            if(r==n-1 && c==m-1) return d;
             for(int i=0;i<4;i++){
-                int x=dx[i]+cx, y=dy[i]+cy;
-                if(isValid(x,y) && !vis[x][y]){
-                     if (abs(heights[cx][cy] - heights[x][y]) <= k){
-                         vis[x][y]=1;
-                         q.push({x,y});
-                     }
+                int x=r+dx[i], y=c+dy[i];
+                if(isValid(x,y)){
+                    int diff=max(d, abs(heights[r][c]-heights[x][y]));
+                    if(diff<dis[x][y]){
+                        dis[x][y]=diff;
+                        pq.push({diff,{x,y}});
+                    }
                 }
             }
         }
         return 0;
-    }
-    
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int lo=0, hi=1e6, ans=-1;
-        n=heights.size(), m=heights[0].size();
-        while(lo<=hi){
-            int mid = lo + (hi - lo) / 2;
-            if(bfs(heights, mid)){
-                ans=mid;
-                hi=mid-1;
-            }
-            else
-                lo=mid+1;
-        }
-        return ans;
     }
 };
