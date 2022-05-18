50f576462b2fd2b111652e7ddf38a844b091cb3d
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-05-18 09:47:35 +0530

    Time: 932 ms (44.06%), Space: 164.3 MB (93.14%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    vector<vector<int>> graph;
+    vector<int> ranks;
+
+    
+    int dfs(int node, int rank, vector<vector<int>>& res) {
+        if (ranks[node] >= 0) return ranks[node];
+
+        ranks[node] = rank;
+        int minRank = rank;
+        
+        for (auto neighbor : graph[node]) {
+            if (ranks[neighbor] == rank - 1 || ranks[neighbor] > rank) continue;
+
+            int neighborRank = dfs(neighbor, rank + 1, res);
+            minRank = min(minRank, neighborRank);
+            if (neighborRank > rank) res.push_back({node, neighbor});
+        }
+        return minRank;
+    }
+    
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        graph.resize(n);
+        for (auto& it : connections) {
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
+        }
+        
+        ranks.resize(n, -2);
+        vector<vector<int>> res;
+        dfs(0, 0, res);        
+        return res;
+    }
+};
