16a52c7c9ab7f43a253ec00ad3ae4d52b14365f4
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-05-20 10:07:18 +0530

    Time: 3 ms (80.74%), Space: 7.5 MB (98.16%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -5,16 +5,21 @@ public:
         if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)
             return 0;
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-        dp[1][1]=1;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(obstacleGrid[i-1][j-1]==1)
-                    dp[i][j]=0;
-                else
-                    dp[i][j]+=dp[i-1][j] + dp[i][j-1];
+        vector<vector<int>> dp(2, vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                dp[i%2][j]=0;
+                if(obstacleGrid[i][j]==1) 
+                    continue;
+                
+                if(i) 
+                    dp[i%2][j]+=dp[(i-1)%2][j];
+                if(j) 
+                    dp[i%2][j]+=dp[i%2][j-1];
             }
         }
-        return dp[m][n];
+        return dp[(m-1)%2][n-1];
     }
 };
