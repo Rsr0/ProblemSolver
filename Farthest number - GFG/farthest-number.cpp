fdd0a60259cb83e496458378847c42d60af7ea1d
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-05-21 09:53:14 +0530

    Added solution - LeetHub

--- Farthest number - GFG/farthest-number.cpp
+++ Farthest number - GFG/farthest-number.cpp
@@ -9,15 +9,29 @@ class Solution{
   public:
     vector<int> farNumber(int N,vector<int> Arr){
         //code here
-        vector<int> ans(N,-1);
-        for(int i=0;i<N-1;i++){
-            for(int j=N-1;j>i;j--){
-                if(Arr[i]>Arr[j]){
-                    ans[i]=j;
-                    break;
-                }
-            }
+        vector<int> ans(N);
+        vector<int> min_right(N);
+        min_right[N-1]=Arr[N-1];
+        
+        for(int i=N-2;i>=0;i--){
+            min_right[i]=min(Arr[i], min_right[i+1]);    
         }
+        
+        for(int i=0;i<N;i++){
+            int lo=i+1, hi=N-1, idx=-1;
+            
+            while(lo<=hi){
+                int mid=(lo+hi)/2;
+                if(min_right[mid]<Arr[i]){
+                    idx=mid;
+                    lo=mid+1;
+                }
+                else
+                    hi=mid-1;
+            }
+            ans[i]=idx;
+        }
+        
         return ans;
     }
 };
