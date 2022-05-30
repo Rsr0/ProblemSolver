0d2e25d827d8e9b9bc5059927b065559e5abd593
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-05-30 14:56:51 +0530

    Time: 544 ms (51.22%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -2,11 +2,11 @@
 SELECT product_id, "store1" AS store, store1 AS price FROM Products
 WHERE store1 is not null
 
-UNION
+UNION ALL
 SELECT product_id, "store2" AS store, store2 AS price FROM Products
 WHERE store2 is not null
 
-UNION
+UNION ALL
 SELECT product_id, "store3" AS store, store3 AS price FROM Products
 WHERE store3 is not null
 
