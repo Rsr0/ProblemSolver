dad4c8a1714d273eaa174ae476decdea131e9d49
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-06-01 19:23:51 +0530

    Time: 1531 ms (23.34%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -2,7 +2,7 @@
 
 SELECT name FROM SalesPerson 
 WHERE sales_id NOT IN(
-                        SELECT sales_id FROM Orders
+                        SELECT DISTINCT sales_id FROM Orders
                         WHERE com_id = (
                                         SELECT com_id FROM Company
                                          WHERE name='RED'
