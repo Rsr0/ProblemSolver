79427ca846affcc44335bb76c9166c882bae8ae5
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-06-12 14:20:05 +0530

    Added solution - LeetHub

---
+++ Count digit groupings of a number - GFG/count-digit-groupings-of-a-number.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    int n;
+    int presum[105] = {0};
+    
+	public:
+	
+	int rec(int ind, int prev, string s, vector<vector<int>>& dp){
+	    if(ind == n)    return 1;
+	    if(dp[ind][prev] != -1)     return dp[ind][prev];
+	    
+	    int res = 0;
+	    for(int i=ind; i<n;i++){
+	        int sum = presum[i+1] - presum[ind];
+	        if(sum >= prev)     res += rec(i+1, sum, s, dp);
+	    }
+	    return dp[ind][prev] = res;
+	}
+    
+	int TotalCount(string str){
+	    // Code here
+	    n = str.length();
+	    for(int i=1;i<=n;i++)  
+	        presum[i] = presum[i-1] + (str[i-1]-'0');
+	    
+	    vector<vector<int>> dp(n, vector<int>(1000, -1));
+	    return rec(0, str[0]-'0', str, dp);
+	}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution ob;
+		int ans = ob.TotalCount(str);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
