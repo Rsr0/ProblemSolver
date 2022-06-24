ddd7cc6c7a37475c7cbe798ffd1bb90c37eeda54
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-06-24 10:55:11 +0530

    Added solution - LeetHub

---
+++ Finding Profession - GFG/finding-profession.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    char profession(int level, int pos){
+        // code here
+        int c=0;
+        pos--;
+        while(pos){
+            pos=pos&(pos-1);
+            c++;
+        }
+        return c%2 ? 'd':'e';
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int level, pos;
+        cin>>level>>pos;
+        
+        Solution ob;
+        if(ob.profession(level, pos) == 'd')
+            cout<<"Doctor\n";
+        else
+            cout<<"Engineer\n";
+    }
+    return 0;
+}  // } Driver Code Ends
