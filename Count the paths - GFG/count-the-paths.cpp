4d6f2ba7840a98708d009751105efcdb308cde96
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-07-02 10:40:57 +0530

    Added solution - LeetHub

--- Count the paths - GFG/count-the-paths.cpp
+++ Count the paths - GFG/count-the-paths.cpp
@@ -14,17 +14,19 @@ void dfs(vector<vector<int>> &adj, int s, int d, int n){
         
     }
     // if(s>=n) return;
-    for(int i=0;i<adj.size();i++){
-        if(adj[i][0]==s)
-        dfs(adj, adj[i][1], d, n);
+    for(int i=0;i<adj[s].size();i++){
+        dfs(adj, adj[s][i], d, n);
     }
     
 }
 
 	int possible_paths(vector<vector<int>>edges, int n, int s, int d){
 	    // Code here
-	    
-	    dfs(edges, s, d, n);
+	    vector<vector<int>> adj(n);
+	    for(int i=0;i<edges.size();i++){
+	        adj[edges[i][0]].push_back(edges[i][1]);
+	    }
+	    dfs(adj, s, d, n);
 	    return cnt;
 	}
 };
