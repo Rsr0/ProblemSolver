d9a7e4e217a43edd278f5165efe0f1479dd44c14
Sahil Rana <51770809+Rsr0@users.noreply.github.com>
2022-07-02 18:28:27 +0530

    Time: 38 ms (75.55%), Space: 6.9 MB (97.85%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        int start=0, end=0, mx=1;
-        if(n<=1)    return s;
+        int n=s.size();
+        if(n<=1) return s;
         
-        //Odd length
-            for(int i=0; i<n-1;i++){
-            int l=i, r=i, len;
-                while(l>=0 && r<n){
-                    if(s[l]==s[r])
-                        l--, r++;
-                    else
-                        break;
-                    
-                }
-                len=r-l-1;
-                if(len > mx)
-                {
-                    mx=len;
-                    start=l+1;
-                    end=r-1;
-                }
+        int start=0, mx=0;
+        // odd 
+        for(int i=0;i<n-1;i++){
+            int l=i, r=i;
+            while(l>=0 && r<n){
+                if(s[l]==s[r])
+                    l--, r++;
+                else 
+                    break;
             }
-        
-        //Even length
-             for(int i=0; i<n-1;i++){   //"bb"
-            int l,r, len;
-                l=i; r=i+1;
-                while(l>=0 && r<n){
-                    if(s[l]==s[r])
-                        l--, r++;
-                    else
-                        break;
-                    
-                }
-                len=r-l-1;
-                if(len > mx)
-                {
-                    mx=len;
-                    start=l+1;
-                    end=r-1;
-                }
+            int len=r-l-1;
+            if(len>mx){
+                start=l+1;
+                mx=len;
             }
+        }
         
-        return(s.substr(start,mx));
+        //even
+        for(int i=0;i<n-1;i++){
+            int l=i, r=i+1;
+            while(l>=0 && r<n){
+                if(s[l]==s[r])
+                    l--, r++;
+                else 
+                    break;
+            }
+            int len=r-l-1;
+            if(len>mx){
+                start=l+1;
+                mx=len;
+            }
+        }
+        
+        return s.substr(start,mx);
     }
-        
 };
